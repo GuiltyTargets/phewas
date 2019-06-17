757243da2335674364661e217133df476b415ac0
Mauricio Lacerda <mauriciopl@gmail.com>
2019-06-17 11:42:17 +0100

    Implementation of Biased SVM for classification.

--- src/guilty_phewas/pulearn.py
+++ src/guilty_phewas/pulearn.py
@@ -5,7 +5,7 @@ from GAT2VEC.evaluation.classification import Classification
 from GAT2VEC import parsers, paths
 import pandas as pd
 from sklearn import svm
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, roc_auc_score
 from sklearn.model_selection import StratifiedKFold
 
 # TODO move to GAT2VEC (forked)?
@@ -14,7 +14,16 @@ from sklearn.model_selection import StratifiedKFold
 class PULearn(Classification):
     """ """
 
-    def evaluate(self, model, label=False, evaluation_scheme="tr"):
+    def evaluate(self, model, label=False, evaluation_scheme="tr", cost_p: float = None, cost_n: float = None):
+        """Evaluates the model according to the given evaluation scheme.
+
+        :param model:
+        :param label:
+        :param evaluation_scheme:
+        :param cost_p: The cost given to the positive values # TODO cost or weight??
+        :param cost_n: The cost given to the negative values # TODO cost or weight??
+        :return:
+        """
         embedding = 0
         clf = self.get_classifier()
 
@@ -22,7 +31,10 @@ class PULearn(Classification):
             embedding = parsers.get_embeddingDF(model)
 
         if evaluation_scheme == "bsvm":
-            results = self.evaluate_bsvm(clf, embedding, 5)
+            if cost_n and cost_p and cost_n > 0 and cost_p > 0:
+                results = self.evaluate_bsvm(embedding, 5, cost_p=cost_p, cost_n=cost_n)
+            else:
+                raise ValueError("Biased SVM requires both negative and positive weights.")
         if evaluation_scheme == "cv":
             results = self.evaluate_cv(clf, embedding, 5)
         elif evaluation_scheme == "tr" or label:
@@ -33,14 +45,14 @@ class PULearn(Classification):
                     model = paths.get_embedding_path_wl(self.dataset_dir, self.output_dir, tr)
                     if isinstance(model, str):
                         embedding = parsers.get_embeddingDF(model)
-                results.update(self.evaluate_tr(clf, embedding, tr))
+                results.update(self.evaluate_tr(clf, embedding,  tr))
 
         print("Training Finished")
 
         df = pd.DataFrame(results)
         return df.groupby(axis=0, by="TR").mean()
 
-    def evaluate_bsvm(self, clf, embedding, n_splits, cost_p=0.5, cost_n=200):
+    def evaluate_bsvm(self, embedding, n_splits, cost_p=.5, cost_n=200.):
         """Implement a biased SVM classifier.
 
         :param clf: Classifier object
@@ -51,13 +63,15 @@ class PULearn(Classification):
         :return: Dictionary containing numerical results of the classification.
         """
 
+        embedding = embedding[self.label_ind, :]
         results = defaultdict(list)
-        clf = svm.SVC()
+        clf = svm.SVC(probability=True)
         for i in range(10):
             rskf = StratifiedKFold(n_splits=n_splits, shuffle=True)
             for train_idx, test_idx in rskf.split(embedding, self.labels):
                 X_train, X_test, Y_train, Y_test = self._get_split(embedding, test_idx, train_idx)
-                pred, probs = self.get_biased_predictions(clf, X_train, X_test, Y_train, Y_test)
+                pred, probs = self.get_biased_predictions(clf, X_train, X_test, Y_train, cost_p, cost_n)
+                print({0: len([_ for x in pred if x == 0]), 1: len([_ for x in pred if x == 1])})
                 results["TR"].append(i)
                 results["accuracy"].append(accuracy_score(Y_test, pred))
                 results["f1micro"].append(f1_score(Y_test, pred, average='micro'))
@@ -69,8 +83,8 @@ class PULearn(Classification):
         return results
 
     @staticmethod
-    def get_biased_predictions(clf, X_train, X_test, Y_train, Y_test, cost_p, cost_n):
-        sample_weight = [cost_p if x == 1 else cost_n for x in y]
+    def get_biased_predictions(clf, X_train, X_test, Y_train, cost_p, cost_n):
+        sample_weight = [cost_p if x == 1 else cost_n for x in Y_train]
         clf.fit(X_train, Y_train, sample_weight)
         return clf.predict(X_test), clf.predict_proba(X_test)
 
@@ -83,7 +97,7 @@ if __name__ == '__main__':
     walk_length = 4
     num_walks = 30
     window_size = 10
-    dimension = 256
+    dimension = 128
 
     model = g2v.train_gat2vec(
         num_walks,
@@ -92,6 +106,8 @@ if __name__ == '__main__':
         window_size,
         output=True,
     )
+    print('***** model obtained *****.')
     pul = PULearn(dir_, dir_, tr=[0.1, 0.3, 0.5])
+    print('PU created.')
     auc_df = pul.evaluate(model, label=False, evaluation_scheme="bsvm")
     print(auc_df)
