7a7024d148e5ecacf879c7585c5a3e52eedc3082
mauriciopl@gmail.com <mauriciopl@gmail.com>
2020-01-26 15:53:09 +0100

    Script to start all runs.

---
+++ run.sh
@@ -0,0 +1,10 @@
+python src/scripts/network_comparisons.py
+python src/scripts/compare_phewas.py
+python src/scripts/compare_svm_rr.py
+python src/scripts/compare_weighted_unweighted.py
+python src/scripts/compare_gat2vec_hyperparams_adjust.py
+python src/scripts/link_prediction.py
+python src/scripts/make_plots.py
+python src/scripts/significance.py
+
+
